5221aded69b9a3d1f197f4d2cbc43ddb95c824a6
Nikita Lyovkin <lyovkin@i.ua>
2019-06-02 18:07:37 +0300

    move users-api to Python, add postgres

---
+++ users-api/src/main.py
@@ -0,0 +1,89 @@
+import os
+
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+import jwt
+
+
+app = Flask('users-api')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    firstname = db.Column(db.String)
+    lastname = db.Column(db.String)
+    role = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<User self.username>'
+
+
+db.create_all()
+db.session.add(
+    User(
+        username='admin',
+        firstname='Foo',
+        lastname='Bar',
+        role=True
+    )
+)
+db.session.add(
+    User(
+        username='johnd',
+        firstname='John',
+        lastname='Doe',
+        role=False
+    )
+)
+db.session.add(
+    User(
+        username='janed',
+        firstname='Jane',
+        lastname='Doe',
+        role=False
+    )
+)
+db.session.commit()
+
+
+@app.before_request
+def validate_jwt():
+    auth_h = request.headers.get('Authorization')
+    if not auth_h:
+        return 'No authorization provided', 401
+
+    if not auth_h.startswith('Bearer '):
+        return 'Unknown token type', 401
+
+    token = auth_h[7:]
+    try:
+        jwt.decode(token, os.getenv('JWT_SECRET'), algorithms='HS256')
+    except jwt.ExpiredSignatureError:
+        return 'JWT is expired', 401
+
+
+@app.route('/users/<username>/')
+def show_user_profile(username):
+    user = User.query.filter(User.username == username).first()
+
+    if not user:
+        return f'No such user: {username}', 404
+
+    result = {
+        'username': user.username,
+        'firstname': user.firstname,
+        'lastname': user.lastname,
+        'role': 'ADMIN' if user.role else 'USER'
+    }
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 8083)
